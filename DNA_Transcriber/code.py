f45fe7f274098678498223d4971b95e7f188197e
informatics_Shubhang <shubhang.biotech.de@gmail.com>
2025-07-24 10:27:34 +0530

    code.py

--- DNA_Transcriber/code.py
+++ DNA_Transcriber/code.py
@@ -1,8 +1,9 @@
 # Input the DNA sequence when prompted 
 input_01 = input("Enter the DNA Sequence for Transcription\n").upper()
 input_02 = input_01.strip().replace(" ", "" )
+valid_bases = ['A', 'T', 'G', 'C']
 for bases in input_02:
-    if bases not in input_02:
+    if bases not in valid_bases:
         print("Invalid DNA Sequence")
 else:
     var1 = input_02.replace("T", "U")
